7e49365801570f629df470d0d034cb17afc73968
Mat Jordan <mat@northwestern.edu>
2025-01-28 15:10:23 -0500

    Update env vars.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
+const isProduction = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: isProduction ? process.env.NEXT_PUBLIC_BASE_PATH : "",
+  assetPrefix: isProduction ? process.env.NEXT_PUBLIC_URL : "",
+  publicRuntimeConfig: {
+    url: isProduction ? process.env.NEXT_PUBLIC_URL : "",
+  },
 };
 
 export default nextConfig;
